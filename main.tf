c5fbabc0c2a54b84c5ec76d91c3143b8c93df9c5
DLaMott <dylanlamott@gmail.com>
2024-09-17 00:15:06 -0400

    feat: adjust

--- main.tf
+++ main.tf
@@ -33,17 +33,14 @@ resource "aws_instance" "blog" {
 # Uses the AWS SG Provider Module
 module "blog_sg" {
   source   = "terraform-aws-modules/security-group/aws"
-  version  = "5.2.0"
-  name     = "blog"
+  version = "4.13.0"
 
   vpc_id  = data.aws_vpc.default.id
-
-
-  ingress_rules        = ["http-80-tcp"," https-443-tcp"]
-  ingress_cidr_blocks  = ["0.0.0.0/0"]
-
-  egress_rules        = ["all-all"]
-  egress_cidr_blocks  = ["0.0.0.0/0"]
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
 
